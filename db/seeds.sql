dd851861acc6d80c597646dbbd8191054549b46b
Avery Brown-Gathings <72925420+averyjbrown2@users.noreply.github.com>
2020-12-12 14:47:57 -0500

    added db folder and populated burger table with values

---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+### populates burgers table with the burger_name value
+INSERT INTO burgers (burger_name) VALUES ('Bacon Cheeseburger');
+INSERT INTO burgers (burger_name) VALUES ('Grilled Chicken');
+INSERT INTO burgers (burger_name) VALUES ('BLT');
