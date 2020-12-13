5884790ab28e31e20fbdb53de5e82bc9ae0df862
Avery Brown-Gathings <72925420+averyjbrown2@users.noreply.github.com>
2020-12-13 12:03:28 -0500

    added use statement to seeds.sql

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,4 +1,5 @@
 ### populates burgers table with the burger_name value
+USE burgers_db;
 INSERT INTO burgers (burger_name) VALUES ('Bacon Cheeseburger');
 INSERT INTO burgers (burger_name) VALUES ('Grilled Chicken');
 INSERT INTO burgers (burger_name) VALUES ('BLT');
