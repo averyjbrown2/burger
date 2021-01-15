772d03267be1bb3aae91cd5433f27f22f9fa34b6
Avery Brown-Gathings <72925420+averyjbrown2@users.noreply.github.com>
2021-01-15 14:47:23 -0500

    added function to burger.js

--- public/assets/js/burger.js
+++ public/assets/js/burger.js
@@ -1,16 +1,31 @@
-$(".change-devoured").on("click", function() {
-    var id = $(this).data("id");
-    console.log(id);
-    var devoured = $(this).data("newdevoured");
-    console.log(devoured);
-    $.ajax({
-        url: "/api/burgers/" + id,
-        type: "PUT",
-        data: {
-          devoured: devoured  
-        }
-    })
-})
+$(".change-devoured").on("click", function () {
+  var id = $(this).data("id");
+  console.log(id);
+  var devoured = $(this).data("newdevoured");
+
+  console.log(devoured);
+  $.ajax({
+    url: "/api/burgers/" + id,
+    type: "PUT",
+    data: {
+      devoured: newdevoured
+    }
+  }).then(
+    function () {
+      console.log("changed burger to", newdevoured);
+      // Reload the page to get the updated list
+      location.reload();
+
+    }
+
+  );
+});
+
+
+
+
+
+
 
 //auto refresh
 //css
